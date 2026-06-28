c413ec5d7182a36eb2be35ad23573b1c3458498d
ponlponl123 <55607166+ponlponl123@users.noreply.github.com>
2026-06-29 00:22:27 +0700

    feat: add integration constants for Streamlabs redirect URI and event bitmask options

--- src/consts/integration.ts
+++ src/consts/integration.ts
@@ -1,7 +1,8 @@
-export const streamlabs_redirect_uri =
-  process.env.NODE_ENV === "production"
-    ? "https://alertbox.org/app/connections/streamlabs"
-    : "http://localhost:3000/app/connections/streamlabs";
+import { isDev } from "@/config/env";
+
+export const streamlabs_redirect_uri = !isDev
+  ? "https://alertbox.org/app/connections/streamlabs"
+  : "http://localhost:3000/app/connections/streamlabs";
 
 export enum StreamlabsOption {
   STRIPE_PAYMENT_SUCCESS = 0b00000001,
